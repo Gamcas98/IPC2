d0090945a88ad42f97ce27584cb9a5e615b8976f
Gamcas98 <40047389+Gamcas98@users.noreply.github.com>
2018-07-30 01:09:47 -0600

    added interfaz grafica

--- Practica 1/Practica IPC/src/practica/ipc/Calculadora.java
+++ Practica 1/Practica IPC/src/practica/ipc/Calculadora.java
@@ -15,6 +15,8 @@ public class Calculadora {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Interfaz i = new Interfaz();
+        i.setVisible(true);
         // TODO code application logic here
     }
     
